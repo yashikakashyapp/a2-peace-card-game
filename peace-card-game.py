87a876aee493556f64c139b00983dd648823c22a
yashikakashyapp <kashyapyashika16@gmail.com>
2024-10-25 19:47:56 -0400

    Creates deck

--- peace-card-game.py
+++ peace-card-game.py
@@ -6,7 +6,7 @@ ranks = ("2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A")
 suits = ("hearts", "diamonds", "clubs", "spades")
 
 # Create a deck of cards
-deck = 
+deck = ((rank, suit) for rank in ranks for suit in suits)
 # Shuffle the deck 
 
 
